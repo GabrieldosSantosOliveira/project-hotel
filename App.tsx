5e1a8d2f4854c90a6011325f766a7b72d65fa1e7
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 21:24:46 -0400

    feat: Add ThemeProvider and AuthContextProvider

--- App.tsx
+++ App.tsx
@@ -4,18 +4,32 @@ import {
   useFonts
 } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
+import { ActivityIndicator } from 'react-native';
 
+import { Loading } from './src/components/Loading';
+import { StatusBar } from './src/components/StatusBar';
+import { AuthContextProvider } from './src/context/AuthContext';
+import { ThemeProvider } from './src/context/ThemeContext';
+import { useTheme } from './src/hooks/useTheme';
 import { Routes } from './src/routes';
-import { Loading } from './src/screens/Loading';
 import { THEME } from './src/styles/theme';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_700Bold,
     Roboto_400Regular
   });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <ThemeProvider>
+        <AuthContextProvider>
+          <StatusBar
+            translucent
+            backgroundColor="transparent"
+          />
+          {fontsLoaded ? <Routes /> : <Loading />}
+        </AuthContextProvider>
+      </ThemeProvider>
     </NativeBaseProvider>
   );
 }
