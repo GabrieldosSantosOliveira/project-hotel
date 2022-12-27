d0c79a4cc396b731336b0be3fe6e13b2e3c5dc4c
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 21:42:04 -0400

    feat: Add new screens

--- src/routes/Stack.tsx
+++ src/routes/Stack.tsx
@@ -1,12 +1,34 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Home } from './../screens/Home';
-const { Group, Navigator, Screen } = createStackNavigator();
+import { Auth } from '../screens/Auth';
+import { Home } from '../screens/Home';
+import { Register } from '../screens/Register';
+import { Welcome } from '../screens/Welcome';
+import { HomePage } from './../screens/HomePage';
+const { Navigator, Screen } = createStackNavigator();
 
 export const StackRoutes = () => {
   return (
-    <Navigator>
-      <Screen name="Home" component={Home} />
+    <Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Screen name="homepage" component={HomePage} />
+      <Screen name="welcome" component={Welcome} />
+      <Screen name="auth" component={Auth} />
+      <Screen name="register" component={Register} />
+    </Navigator>
+  );
+};
+export const StackRoutesNoAuth = () => {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Screen name="home" component={Home} />
     </Navigator>
   );
 };
